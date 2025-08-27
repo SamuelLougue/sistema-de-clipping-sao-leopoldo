20fab6f242aada97e7b0e6991ee6e62cb9bd7bf5
Samuel Lougue <156086460+SamuelLougue@users.noreply.github.com>
2025-08-27 13:35:30 -0300

    Add .next/static/chunks/src_app_admin_page_tsx_a993527f._.js 1756312439664

---
+++ .next/static/chunks/src_app_admin_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_394b32fb._.js",
+  "static/chunks/node_modules_614cf6cd._.js"
+],
+    source: "dynamic"
+});
