84b2ed5908f66d896c701af42673d7501e2fcf76
Samuel Lougue <156086460+SamuelLougue@users.noreply.github.com>
2025-08-27 13:35:39 -0300

    Add .next/static/chunks/src_app_clipping-dashboard_page_tsx_a993527f._.js 1756312448871

---
+++ .next/static/chunks/src_app_clipping-dashboard_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_a27a612a._.js",
+  "static/chunks/node_modules_bdf388f7._.js"
+],
+    source: "dynamic"
+});
